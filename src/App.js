eae44ecd0b61d34aeb018345df577c7faae2dfcc
rod080 <49286063+rod080@users.noreply.github.com>
2019-09-23 23:52:51 -0700

    lets git it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm ] = useState('');
+  const onInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  let API_URL = `https://www.googleapis.com/books/v1/volumes`;
+
+  const [books, setBooks] = useState({ items: [] });
+
+  const fetchBooks = async () => {
+    const result = await axios.get(`${API_URL}?q=${searchTerm}`);
+    console.log(result.data);
+    setBooks(result.data);
+  }
+  const onSubmitHandler = (e) => {
+    e.preventDfault();
+    fetchBooks();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form onSubmit = {onSubmitHandler} >
+        <label>
+          <span>Search for a book!</span>
+          <input
+            type="search"
+            placeholder="microservice, restful design, etc.,"
+            value = {setSearchTerm}
+            onChange = {onInputChange}
+          />
+          <button type="submit">Search</button>
+        </label>
+      </form>
+      <ul>
+        {
+          books.items.map((book, index) => {
+            return (
+              <li key={index}>
+                <div>
+                  <img alt={`${book.volumeInfo.title} book`} src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} />
+                  <div>
+                    <h3>{book.volumeInfo.title}</h3>
+                    <p>{book.volumeInfo.publishedDate}</p>
+                  </div>
+                </div>
+                <hr />
+              </li>
+            );
+          })
+        }
+      </ul>
+    </section>
+    
   );
-}
+};
 
 export default App;
